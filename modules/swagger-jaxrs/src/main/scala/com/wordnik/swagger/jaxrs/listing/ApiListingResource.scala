ca0c5abf416b794b6288bf0ffa4938de8a7f71c0
Chad Hahn <chad@codegoop.com>
2013-02-19 20:54:46 -0500

    Updated ApiListing Path to '/api-docs' dropping json out of '/api-docs.json' as the default

--- modules/swagger-jaxrs/src/main/scala/com/wordnik/swagger/jaxrs/listing/ApiListingResource.scala
+++ modules/swagger-jaxrs/src/main/scala/com/wordnik/swagger/jaxrs/listing/ApiListingResource.scala
@@ -5,7 +5,9 @@ import com.wordnik.swagger.jaxrs._
 
 import javax.ws.rs._
 
-@Path("/api-docs.json")
+//Dropping the .json as the default for now. Think that should be the standard
+//@Path("/api-docs.json")
+@Path("/api-docs")
 @Api("/api-docs")
 @Produces(Array("application/json"))
 class ApiListingResourceJSON extends ApiListing
